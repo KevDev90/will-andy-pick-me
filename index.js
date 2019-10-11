798b93620328889c1f93cc103b15a33655a9ff23
kevin johnson <kevinj902302@gmail.com>
2019-10-11 12:08:04 -0600

    updated version but random string functionality is still not working

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ var possibleAnswers = [
   "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now",
  "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
  "My sources say no", "Outlook not so good", "Very doubtful"
-]
+];
+var getAnswerButton = document.getElementById('get-your-answer-button');
+var input  = document.getElementById('ask-your-question');
+var main = document.querySelector('.main');
+var main2 = document.querySelector('.main-2');
 
-fetchQuote(p)
-$("#get-your-answer-button").click(function() {fetchQuote(possibleAnswers));
-  function fetchQuote(possibleAnswers) {
-   var quoteNum = Math.floor(Math.random() * possibleAnswers.length);
-   $(#possibleAnswersDisplay").text("-" + q[quoteNum] + ' "' );
 
-  }
-});  
+ function getAnswer() {
+  var randomAnswer = possibleAnswers[Math.floor(Math.random() * 20)];
+  document.querySelector('.random-answer').innerHTML = possibleAnswers[randomAnswer];
+};
+
+
+getAnswerButton.addEventListener('click', getRandomAnswer)
+
+function getRandomAnswer() {
+  main.classList.add('hidden');
+  main2.classList.remove('hidden');
+  document.querySelector('.their-question').innerHTML = input.value;
+  getAnswer();
+
+
+}
